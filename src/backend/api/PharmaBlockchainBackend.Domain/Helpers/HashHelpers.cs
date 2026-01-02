1a533f336c633ed747c4027c2a00542760bfa5ff
Coretex <valerash2@gmail.com>
2026-01-02 10:10:10 +0200

    hash helper modification

--- src/backend/api/PharmaBlockchainBackend.Domain/Helpers/HashHelpers.cs
+++ src/backend/api/PharmaBlockchainBackend.Domain/Helpers/HashHelpers.cs
@@ -1,14 +1,22 @@
 ﻿using PharmaBlockchainBackend.Domain.Enums;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace PharmaBlockchainBackend.Domain.Helpers
 {
     public static class HashHelpers
     {
+        private static readonly JsonSerializerOptions _jsonOptions = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false,
+            DefaultIgnoreCondition = JsonIgnoreCondition.Never
+        };
         public static byte[] CalculateHash(ProtocolType protocolType, int stepNumber, Guid packageCode, object? additionalData)
         {
             var additionalDataString = additionalData is null
                 ? string.Empty
-                : System.Text.Json.JsonSerializer.Serialize(additionalData);
+                : JsonSerializer.Serialize(additionalData, _jsonOptions);
 
             var rawData = $"{protocolType}|{stepNumber}|{additionalDataString}|{packageCode}";
             var bytes = System.Security.Cryptography.SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(rawData));
